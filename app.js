9f4572afbc401ed425768441720c8dbf9dcf655f
Yuval Akavian <yuvalmetal@gmail.com>
2023-09-29 14:59:39 +0300

    added posts template

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ mongoose.connect(process.env.MONGO_URL, {
 });
 
 app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/views/posts'));
+
 
 // Middleware
 app.use(cors());
