67e17a1efd326e07a10856b5b35ef65cec229bea
Raz Gavriel <= raz5338@gmail.com>
2023-10-03 16:33:10 +0300

    fix code structure

--- views/users/users.js
+++ views/users/users.js
@@ -1,78 +1,69 @@
-const onEnter = async (event) => {
-    if (event.key === 'Enter') {
-        const searchValue = document.getElementById("searchUsers").value.toLowerCase();
-
-        $.ajax({
+$(document).ready(function () {
+    const getUser = () => {
+        return $.ajax({
             url: "/users/get-user",
-            method: "GET",
-            data: {},
-            success: function (data) {
-                const currentUser = data;
-                console.log(data);
-
-                $.ajax({
-                    url: "/users/search-users",
-                    method: "POST",
-                    data: { searchValue: searchValue },
-                    success: function (data) {
-                        const filteredUsersContainer = document.getElementById(
-                            "filteredUsersContainer"
-                        );
-                        filteredUsersContainer.innerHTML = "";
-        
-                        data?.filter(user => user._id != currentUser._id).forEach((user) => {
-                            const userElement = document.createElement("div");
-                            userElement.classList.add("d-flex", "justify-content-between", "align-items-center");
-        
-                            const nameElement = document.createElement("span");
-                            nameElement.textContent = `${user.firstName} ${user.lastName}`;
-        
-                            userElement.appendChild(nameElement);
-
-                            if (!currentUser?.friends.includes(user._id)) {
-                                const buttonElement = document.createElement("button");
-                                buttonElement.textContent = "Add friend";
-                                buttonElement.classList.add("btn", "btn-primary");
-                                buttonElement.addEventListener('click', () => {
-                                    console.log('click');
-                                    $.ajax({
-                                        url: "/users/add-friend",
-                                        method: "POST",
-                                        data: { friendId: user._id },
-                                        success: function (data) {
-                                            console.log(data);
-                                        },
-                                        error: function (error) {
-                                            console.log(error);
-        
-                                        }
-                                    });
-                                });
-        
-                                userElement.appendChild(buttonElement);
-                            } else {
-                                const buttonElement = document.createElement("button");
-                                buttonElement.textContent = "Friend";
-                                buttonElement.classList.add("btn", "btn-success", "disabled");
-                                userElement.appendChild(buttonElement);
-                            }
-        
-                            userElement.style.marginBottom = "10px";
-        
-                            filteredUsersContainer.appendChild(userElement);
-        
-                        });
-                    },
-                    error: function (error) {
-                        console.log(error);
-        
-                    }
-                });
-            },
-            error: function (error) {
-                console.log(error);
-
-            }
+            method: "GET"
         });
-    }
-}
+    };
+
+    const searchUsers = (searchValue) => {
+        return $.ajax({
+            url: "/users/search-users",
+            method: "POST",
+            data: { searchValue: searchValue }
+        });
+    };
+
+    const addFriend = (friendId) => {
+        return $.ajax({
+            url: "/users/add-friend",
+            method: "POST",
+            data: { friendId: friendId }
+        });
+    };
+
+    const renderUserElement = (user, currentUser, container) => {
+        const userElement = $("<div>").addClass("d-flex justify-content-between align-items-center");
+        const nameElement = $("<span>").text(`${user.firstName} ${user.lastName}`);
+        userElement.append(nameElement);
+
+        const buttonElement = $("<button>").addClass("btn btn-primary");
+        if (!currentUser?.friends.includes(user._id)) {
+            buttonElement.text("Add friend");
+            buttonElement.on('click', async () => {
+                try {
+                    await addFriend(user._id);
+                    console.log('Friend added successfully.');
+                } catch (error) {
+                    console.log(error);
+                }
+            });
+        } else {
+            buttonElement.text("Friend").addClass("btn-success disabled");
+        }
+        userElement.append(buttonElement);
+        userElement.css("margin-bottom", "10px");
+        container.append(userElement);
+    };
+
+    const onEnter = async (event) => {
+        if (event.key === 'Enter') {
+            const searchValue = $("#searchUsers").val().toLowerCase();
+            const currentUser = await getUser();
+
+            try {
+                const data = await searchUsers(searchValue);
+                const filteredUsersContainer = $("#filteredUsersContainer");
+                filteredUsersContainer.empty();
+
+                data?.filter(user => user._id !== currentUser._id).forEach((user) => {
+                    renderUserElement(user, currentUser, filteredUsersContainer);
+                });
+            } catch (error) {
+                console.log(error);
+            }
+        }
+    };
+
+    $("#searchUsers").on('keydown', onEnter);
+});
